7a7369152272ccc19308b8d58ae9d51fba07c7f3
surendrakumar6350 <kamleshksks456@gmail.com>
2024-02-16 16:42:21 -0800

    bug

--- app/api/v1/proids/route.js
+++ app/api/v1/proids/route.js
@@ -12,8 +12,8 @@ try {
         status: 200,
         success: true,
         productids : [
-            "65ca0f812fc2e0f4170e432e","65ca0f812fc2e0f4170e432e","65ca0f812fc2e0f4170e432e","65ca0f812fc2e0f4170e432e","65ca0f812fc2e0f4170e432e",
-            "65ca0f812fc2e0f4170e432e","65ca0f812fc2e0f4170e432e","65ca0f812fc2e0f4170e432e"
+            "65cfe7a2d2d2dc7d3ea162b3","65cfe5d71e2005607dfc4ec1","65cfe26a645bf555018d5787","65cfea8663d2b49aa7fde2f1","65b1011d1d2ebe4997300003",
+            "65cc1db24cd5cf40a8c748a8","65ca0f812fc2e0f4170e432e","65ca0f812fc2e0f4170e432e"
           ]
     })
 }
